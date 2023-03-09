4093f1d39d4e22e5616c2a68d751c26df4bbbdda
gerrp <119976120+gerrp@users.noreply.github.com>
2023-03-08 21:13:23 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+        document.getElementById('boton').onclick = function() {
+            document.getElementById("numeroTel").style.display="block";
+
+        }
+        document.getElementById("botonMail").onclick= function(){
+            document.getElementById("direcMail").style.display="block";
+
+        }
+        document.getElementById("botonDirec").onclick= function(){
+            document.getElementById("direcPersonal").style.display="block";
+        }
+        document.getElementById("botonEdad").onclick=function(){
+            document.getElementById("edad").style.display="block";
+        }
